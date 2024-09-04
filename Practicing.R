77906c94355aafe9b85bd4f655b32a98b26524bf
SuyongYang <hifivetome27@gmail.com>
2024-09-05 01:01:52 +0900

    set env

---
+++ Practicing.R
@@ -0,0 +1,3 @@
+library(hayashir)
+Nerlove = data("nerlove")
+View(Nerlove)
---
+++ SetEnv.R
@@ -0,0 +1,3 @@
+# install devtools if not already installed
+# install.packages(devtools)
+devtools::install_github("lachlandeer/hayashir")
